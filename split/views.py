3cecf08b4a2df6cfbeb170a04b002ef107c61d5c
sanketpayghan <sanket.payghan8@gmail.com>
2018-09-28 18:37:57 +0530

    exception management

--- split/views.py
+++ split/views.py
@@ -27,32 +27,36 @@ class ExpenseView(View):
 		paid - {'user1': amount1, 'user2': amount2}
 		owes = {'user1': amount1, 'user3': amonut2}
 		"""
-		data = request.POST
-		total_paid, total_owes = 0, 0
-		splits = []
-		paid = json.loads(data.get('paid', "{}"))
-		owes = json.loads(data.get('owes', "{}"))
-		for user, amount in paid.iteritems():
-			total_paid = total_paid + Decimal(amount)
-		for user, amount in owes.iteritems():
-			total_owes = total_owes + Decimal(amount)
-		if Decimal(total_paid) != Decimal(data.get('amount')):
-			self.response['message'] = "total amount paid does not match with transaction amount."
-			return HttpResponseBadRequest(json.dumps(self.response), content_type='application/json')
-		if total_owes != Decimal(data.get('amount')):
-			self.response['message'] = "total owned amount does not match with transaction amount."
-			return HttpResponseBadRequest(json.dumps(self.response), content_type='application/json')
-		with transaction.atomic():
-			expense = Expense.objects.create(name=data.get('name'),
-				amount = data.get('amount'), created_by = data.get('created_by'), group=data['group'])
+		try:
+			data = request.POST
+			total_paid, total_owes = 0, 0
+			splits = []
+			paid = json.loads(data.get('paid', "{}"))
+			owes = json.loads(data.get('owes', "{}"))
 			for user, amount in paid.iteritems():
-				splits.append(SplitExpense(user=user,paid=amount,owes=owes.get(user, 0), txn=expense))
-				if user in owes:
-					owes.pop(user)
+				total_paid = total_paid + Decimal(amount)
 			for user, amount in owes.iteritems():
-				splits.append(SplitExpense(user=user,paid=0,owes=amount, txn=expense))
-			SplitExpense.objects.bulk_create(splits)
-		return HttpResponse(json.dumps(self.response), content_type='application/json')
+				total_owes = total_owes + Decimal(amount)
+			if Decimal(total_paid) != Decimal(data.get('amount')):
+				self.response['message'] = "total amount paid does not match with transaction amount."
+				return HttpResponseBadRequest(json.dumps(self.response), content_type='application/json')
+			if total_owes != Decimal(data.get('amount')):
+				self.response['message'] = "total owned amount does not match with transaction amount."
+				return HttpResponseBadRequest(json.dumps(self.response), content_type='application/json')
+			with transaction.atomic():
+				expense = Expense.objects.create(name=data.get('name'),
+					amount = data.get('amount'), created_by = data.get('created_by'), group=data['group'])
+				for user, amount in paid.iteritems():
+					splits.append(SplitExpense(user=user,paid=amount,owes=owes.get(user, 0), txn=expense))
+					if user in owes:
+						owes.pop(user)
+				for user, amount in owes.iteritems():
+					splits.append(SplitExpense(user=user,paid=0,owes=amount, txn=expense))
+				SplitExpense.objects.bulk_create(splits)
+			return HttpResponse(json.dumps(self.response), content_type='application/json')
+		except MultiValueDictKeyError as e:
+			self.response['message'] = 'Mandatory input parameter missing.'
+			return HttpResponseBadRequest(json.dumps(self.response), content_type='application/json')
 
 	def get(self, request):
 		group = request.GET.get('group')
