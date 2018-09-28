cd55270dc7218c3939151390e693d4bb2e9168d8
sanketpayghan <sanket.payghan8@gmail.com>
2018-09-28 14:37:48 +0530

    module name changes

--- split/migrations/0001_initial.py
+++ split/migrations/0001_initial.py
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
                 ('user', models.CharField(db_index=True, max_length=255)),
                 ('paid', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('owes', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('txn', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='Split.Expense')),
+                ('txn', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='split.Expense')),
             ],
         ),
     ]
