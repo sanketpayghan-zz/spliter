c9877143f8b423026df218bfed169642463d3364
sanket payghan <sanket.payghan@sw-lp02513.bundl.in>
2018-12-18 13:35:39 +0530

    added tests

--- split/tests.py
+++ split/tests.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
-
+from .models import Expense
 # Create your tests here.
+
+class ExpenseTestCase(TestCase):
+    def setup(self):
+        pass
+
+    def test_check(self):
+        self.assertEqual('Same', 'Same')
